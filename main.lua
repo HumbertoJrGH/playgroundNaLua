ad4a37cfdb98754abb9da15337821a88b4d8a655
Humberto Jr <91671380+HumbertoJrGH@users.noreply.github.com>
2025-06-30 09:05:37 -0300

    ajuste main

--- main.lua
+++ main.lua
@@ -2,3 +2,13 @@ require "foo"
 require "generator"
 
 print("eu euin")
+Prompt()
+
+function love.load()
+	love.window.setMode(800, 600, { resizable = false })
+	love.window.setTitle("window")
+end
+
+function love.draw()
+	love.graphics.print("Hello, Lua!", 350, 280)
+end
